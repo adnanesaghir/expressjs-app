50fa5f5d03349f6ab69ecfb6ecb21845ef521f7f
Adnane Saghir <adnane.saghir.ext@descartesunderwriting.com>
2022-08-24 13:49:27 +0200

    dockerize

--- routes.js
+++ routes.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function(req, res, next) {
-  res.end('homepage!')
+  res.end('Welcome to app: ' + process.env.APP_PATH)
 })
 
 router.get('/api', function(req, res, next) {
-  res.json({name: 'routes', version: '1.0'})
+  res.json({name: process.env.APP_PATH, version: '1.0'})
 })
 
 module.exports = router
