9434a4e1d7aff9bc3e8939988a9d1fd1c59602ec
Adnane SAGHIR <6340858+adnanesaghir@users.noreply.github.com>
2023-10-11 10:52:03 +0000

    app service test

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const port = process.env.APP_PORT || 3000
 const routes = require('./routes')
 const app = express()
 
-app.use(express.json())
 app.use('/', routes)
-app.use(process.env.APP_PATH, routes) // handle custom app path
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- routes.js
+++ routes.js
@@ -1,12 +1,32 @@
+const fs = require('fs')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', function(req, res, next) {
-  res.end('Welcome to app: ' + process.env.APP_PATH)
+router.get('/api', function(req, res, next) {
+  const country = req.query.country
+  const action = req.query.action
+  let file
+  if (country === 'europe') {
+    file = '/europe/eu.txt'
+  } else if (country === 'asia') {
+    file = '/asia/asia.txt'
+  } else {
+    return res.end('unknown country!')
+  }
+  if (action === 'write') {
+    const content = new Date().toISOString()
+    fs.writeFileSync('/europe/eu.txt', content, { encoding: 'utf8' })
+    return res.end('done')
+  } else if (action === 'read') {
+    const data = fs.readFileSync('/europe/eu.txt', { encoding: 'utf8', flag: 'r' })
+    return res.end(data)
+  } else {
+    return res.end('unknown action!')
+  }
 })
 
-router.get('/api', function(req, res, next) {
-  res.json({name: process.env.APP_PATH, version: '1.0'})
+router.get('/', function(req, res, next) {
+  res.end('API available in path /api')
 })
 
 module.exports = router
