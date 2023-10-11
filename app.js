cc5f22d9e71d44a36a349106e1f8db75b8d21032
Adnane SAGHIR <6340858+adnanesaghir@users.noreply.github.com>
2023-10-11 11:53:56 +0000

    port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const port = process.env.APP_PORT || 3000
+const port = process.env.APP_PORT || 8080
 const routes = require('./routes')
 const app = express()
 
 app.use('/', routes)
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
